9f8bad2fd0da23b96fb4a5070dc6239048cc05ed
Robert Oliveira <robertsudoliveira@gmail.com>
2021-08-28 16:22:27 -0300

    coloração + tamanho

---
+++ app.js
@@ -0,0 +1,12 @@
+let InputAdultos = document.getElementById("adultos")
+let InputCriancas = document.getElementById("criancas")
+let InputDuracao = document.getElementById("duracao")
+let InputResultado = document.getElementById("resultado")
+
+function calcular(){
+    console.log("calculando...")
+}
+
+
+
+
